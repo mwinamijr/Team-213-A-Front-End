80ce88cbda56adced49704282852e52c41221794
mwinamijr <mwinamijr@gmail.com>
2020-09-07 21:39:34 +0300

    created navbar for tha page

---
+++ src/containers/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Navbar from '../components/Navbar';
+
+const Layout = () => {
+    return (
+    <div>
+        <Navbar />
+    </div>
+    )
+}
+
+export default Layout;
