d30ac573b535e00fcd54cfc9b9f90e1e84f74d65
mwinamijr <mwinamijr@gmail.com>
2020-09-08 11:27:10 +0300

    added react bootstrap

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
+
 
 const HistoryList = () => {
     return(
-        <div>
-            <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-        </div>
+      <tr>
+        <td>1</td>
+        <td>Mark John</td>
+        <td>Babati, Manyara</td>
+        <td>
+          <span className='ml-3'><i class="far fa-eye"></i></span>
+          <span className='ml-3'><i class="fas fa-pencil-alt"></i></span>
+          <span className='ml-3'><i class="fas fa-times-circle"></i></span>
+        </td>
+      </tr>
+        
     )
 } 
 export default HistoryList;
