563f9a1251a0683b8dd04c1cf1adc0f2cf4c64ec
mwinamijr <mwinamijr@gmail.com>
2020-09-13 10:35:50 +0300

    created homepage

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 
 import History from './containers/History';
+import HomePage from './containers/HomePage';
 import HistoryDetail from './containers/Historydetail';
 import HistoryForm from './components/HistoryForm';
 
 const BaseRouter = () => {
     return(
         <div>
+            <Route exact path='/' component={HomePage} />
             <Route exact path='/history' component={History} />
             <Route exact path='/history/:historyID' component={HistoryDetail} />
             <Route exact path='/addHistory' component={HistoryForm} />
